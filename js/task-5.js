8843e07d8963a5cbf6d13d7e460d738753e3a080
Elenagri9580 <150274791+Elenagri9580@users.noreply.github.com>
2024-03-11 23:02:53 +0200

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -5,3 +5,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+const button = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+const handleClick = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor();
+  console.log(body.style.backgroundColor);
+};
+
+button.addEventListener('click', handleClick);
